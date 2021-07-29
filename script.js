55421a2ea55f21f130cb5c3d738d7da5f6a7f011
Miguel Lopez <hello@miguellopez.dev>
2021-07-29 10:32:06 -0700

    Add function to change size and reset grid

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const gridContainer = document.querySelector('.grid__container');
-let gridSize = 64;
+let gridSize = 16;
 
-for (i = 0; i < gridSize * gridSize; i++) {
-	const gridDiv = document.createElement('div');
-	gridDiv.classList.add('grid__row');
-	gridContainer.prepend(gridDiv);
-}
+let makeGrid = function () {
+	for (i = 0; i < gridSize * gridSize; i++) {
+		const gridDiv = document.createElement('div');
+		gridDiv.classList.add('grid__row');
+		gridContainer.prepend(gridDiv);
+	}
+	gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+	const square = document.querySelectorAll('.grid__row');
 
-console.log(gridContainer.classList);
-gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-
-const square = document.querySelectorAll('.grid__row');
-square.forEach((element) => {
-	element.addEventListener('mouseover', function (e) {
-		e.target.style.backgroundColor = 'blue';
+	square.forEach((element) => {
+		element.addEventListener('mouseover', function (e) {
+			e.target.style.backgroundColor = 'blue';
+		});
 	});
-});
+};
+
+const resetGrid = function () {
+	let test = document.querySelectorAll('.grid__row');
+	test.forEach((element) => {
+		element.parentNode.removeChild(element);
+	});
+};
+
+const reset = function () {
+	gridSize = prompt('Pick a grid size of 1 - 100.');
+	resetGrid();
+	makeGrid();
+};
+
+makeGrid();
